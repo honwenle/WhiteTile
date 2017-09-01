1eb99730fe63b24b38ea84e77bfe7b1163f0906c
洪文乐 <noclip@foxmail.com>
2017-09-01 09:03:19 +0800

    监听、画格子

--- main.js
+++ main.js
@@ -6,8 +6,12 @@ cvs.width = WIDTH;
 cvs.height = HEIGHT;
 var SIZE_WIDTH = ~~(WIDTH / 4),
     SIZE_HEIGHT = HEIGHT / 5;
+var luckList = {};
 
-function drawBlock () {}
+function drawBlock (x, y, c) {
+    ctx.fillStyle = c || '#000';
+    ctx.fillRect(x * SIZE_WIDTH + x, y * SIZE_HEIGHT + 1, SIZE_WIDTH, SIZE_HEIGHT - 1);
+}
 function drawRow (y) {
     ctx.strokeStyle = '#000';
     ctx.lineWidth = 1;
@@ -15,8 +19,8 @@ function drawRow (y) {
     ctx.lineTo(WIDTH, SIZE_HEIGHT * y);
     ctx.stroke();
     var luck = ~~(Math.random() * 4);
-    ctx.fillStyle = '#000';
-    ctx.fillRect(luck * SIZE_WIDTH + luck, y * SIZE_HEIGHT + 1, SIZE_WIDTH, SIZE_HEIGHT);
+    drawBlock(luck, y);
+    luckList[y] = luck;
 }
 function drawColLine () {
     ctx.strokeStyle = '#000';
@@ -28,12 +32,18 @@ function drawColLine () {
     }
 }
 drawColLine();
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 6; i++) {
     drawRow(i);
 }
 cvs.addEventListener('touchstart', touchOrClick, false);
 function touchOrClick (e) {
     var x = e.touches[0].clientX,
         y = e.touches[0].clientY;
-    console.log(x,y)
+    x = ~~(x/SIZE_WIDTH);
+    y = ~~(y/SIZE_HEIGHT);
+    if (x == luckList[y]) {
+        drawBlock(x, y, '#666');
+    } else {
+        drawBlock(x, y, '#f00');
+    }
 }
